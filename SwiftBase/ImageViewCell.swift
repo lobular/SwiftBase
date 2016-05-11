1e1793ec7cc45a8de5cac88403e4ef827a834d88
pengxiuxiu <1479432699@qq.com>
2016-05-11 16:23:08 +0800

    cash

--- SwiftBase/ImageViewCell.swift
+++ SwiftBase/ImageViewCell.swift
@@ -29,10 +29,17 @@ class ImageViewCell: UITableViewCell {
     
     func createImage(){
        var image = ["image1.jpg","image2.jpg","image3.jpg","image4.jpg","image5.jpg"]
+        var imageName = ["我","不","吭","不","代","表","我","没","脾","气"]
         for  i in 0..<5{
-            let imageView = UIImageView(frame: CGRectMake( CGFloat(i) * (84 + 10), 0, 84, 84))
+            let imageView = UIImageView(frame: CGRectMake(CGFloat(i) * (84 + 10), 0, 84, 84))
+            let label = UILabel(frame: CGRectMake(CGFloat(i) * (84 + 10), imageView.frame.height - 20 , 84, 20))
+            label.backgroundColor = UIColor(red: 255 / 255.0, green: 193 / 255.0, blue: 205 / 255.0, alpha: 0.8)
+            label.text = imageName[i]
+            label.textColor = UIColor.blackColor()
             imageView.image = UIImage(named: image[i])
+            
            imageScroll.addSubview(imageView)
+           imageScroll.addSubview(label)
         }
         
     }
